a56f59af0645b6e24f3701828a6fea30ee3c0642
Kirill Borisov <lensvol@gmail.com>
2022-07-16 22:20:25 +0200

    Attribute viewport detection snippet.

--- dist/fixers/auto_scroll.js
+++ dist/fixers/auto_scroll.js
@@ -1,9 +1,11 @@
+// Adapted from
+// https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
 function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
+    const rect = el.getBoundingClientRect();
     return (rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */);
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth));
 }
 export class AutoScrollFixer {
     constructor() {
