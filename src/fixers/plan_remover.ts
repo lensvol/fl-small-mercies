9460bbf97fcc83a4816b0ae332beb2ca854cbdb9
Kirill Borisov <lensvol@gmail.com>
2022-11-17 13:34:14 +0100

    Improve plan removal fixer to also know about other button states.

--- src/fixers/plan_remover.ts
+++ src/fixers/plan_remover.ts
@@ -1,7 +1,15 @@
 import {IMutationAwareFixer} from "./base.js";
 import {SettingsObject} from "../settings.js";
 
-const PLAN_BUTTONLET_SELECTOR = "div[class='branch__plan-buttonlet'] button[aria-label='Mark this choice as a plan'"
+const PLAN_ARIA_LABELS = [
+    "Mark this choice as a plan",
+    "You have completed this plan; click to restart it",
+    "Remove this choice from your plans",
+];
+const PLAN_BUTTONLET_SELECTOR = PLAN_ARIA_LABELS.map(
+    (label) => `div.branch__plan-buttonlet button[aria-label='${label}']`
+).join(", ");
+//"div.branch__plan-buttonlet button[aria-label=], div.branch__plan-buttonlet button[aria-label=''], div.branch__plan-buttonlet button[aria-label='']";
 const PLANS_BUTTON_SELECTOR = "li[data-name='plans']"
 
 export class PlanButtonsFixer implements IMutationAwareFixer {
