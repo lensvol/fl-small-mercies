8a8f3bfcf0ad4d756b101ca51f118001d2bd7555
Kirill Borisov <lensvol@gmail.com>
2022-12-13 21:32:47 +0100

    Add capability to track quality changes on a character.

--- src/game_state.ts
+++ src/game_state.ts
@@ -1,10 +1,12 @@
+// @ts-nocheck: There is hell and then there is typing other people's API.
 import {FLApiInterceptor} from "./api_interceptor";
 
 export class UnknownUser {}
 export class UnknownCharacter {}
 
 enum StateChangeTypes {
-    StateChanged
+    StateChanged,
+    QualityChanged,
 }
 
 export class FLUser {
@@ -29,11 +31,43 @@ export class FLCharacter {
     }
 }
 
+export class Quality {
+    qualityId: number;
+    name: string
+    level: number;
+
+    constructor(qualityId: number, category: string, name: string, level: number) {
+        this.qualityId = qualityId;
+        this.category = category;
+        this.name = name;
+        this.level = level;
+    }
+}
+
 export class GameState {
     public user: UnknownUser | FLUser = new UnknownUser();
     public character: UnknownCharacter | FLCharacter = new UnknownCharacter();
 
     public actionsLeft = 0;
+
+    private qualities: Map<string, Map<string, Quality>> = new Map();
+    public getQuality(category: string, name: string): Quality | undefined {
+        const existingCategory = this.qualities.get(category);
+        if (existingCategory == undefined) {
+            return undefined;
+        }
+
+        return existingCategory.get(name)
+    }
+
+    public setQuality(categoryName: string, qualityName: string, quality: Quality) {
+        if (!this.qualities.has(categoryName)) {
+            this.qualities.set(categoryName, new Map());
+        }
+
+        const category = this.qualities.get(categoryName);
+        category.set(qualityName, quality);
+    }
 }
 
 
@@ -41,7 +75,8 @@ export class GameStateController {
     private state: GameState = new GameState();
 
     private changeListeners: { [key in StateChangeTypes]: ((g: GameState) => void)[] } = {
-        [StateChangeTypes.StateChanged]: []
+        [StateChangeTypes.StateChanged]: [],
+        [StateChangeTypes.QualityChanged]: [],
     }
 
     public parseUserResponse(response: Object) {
@@ -59,15 +94,35 @@ export class GameStateController {
         // @ts-ignore: There is hell and then there is writing types for external APIs
         this.state.character = new FLCharacter(response.character.id, response.character.name);
 
-        this.triggerListeners(StateChangeTypes.StateChanged)
+        for (const category of response.possessions) {
+            for (const thing of category.possessions) {
+                const existingQuality = this.state.getQuality(category.name, thing.name);
+
+                if (existingQuality && existingQuality.level != thing.level) {
+                    // We save previous value here so that we can update quality value in-place and still pass it on.
+                    const previousLevel = existingQuality.level;
+                    this.triggerListeners(StateChangeTypes.QualityChanged, existingQuality, previousLevel, thing.effectiveLevel);
+                } else {
+                    const quality = new Quality(thing.id, thing.category, thing.name, thing.level);
+                    this.state.setQuality(category.name, thing.name, quality);
+                    this.triggerListeners(StateChangeTypes.QualityChanged, quality, 0, thing.effectiveLevel);
+                }
+            }
+        }
+
+        this.triggerListeners(StateChangeTypes.StateChanged, this.state);
     }
 
     public onStateChanged(handler: ((g: GameState) => void)) {
         this.changeListeners[StateChangeTypes.StateChanged].push(handler);
     }
 
-    private triggerListeners(changeType: StateChangeTypes): void {
-        this.changeListeners[changeType].map((handler) => handler(this.state))
+    public onQualityChanged(handler: ((quality: Quality, previousLevel: number, currentLevel: number) => void)) {
+        this.changeListeners[StateChangeTypes.QualityChanged].push(handler);
+    }
+
+    private triggerListeners(changeType: StateChangeTypes, ...additionalArgs: any[]): void {
+        this.changeListeners[changeType].map((handler) => handler(...additionalArgs));
     }
 
     public hookIntoApi(interceptor: FLApiInterceptor) {
