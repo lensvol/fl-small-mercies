82362b6550edd4c9e5bec182ad96c613cba7e09a
Kirill Borisov <lensvol@gmail.com>
2023-02-25 12:39:05 +0100

    Properly hide qualities on first page load.

--- src/fixers/location_qualities.ts
+++ src/fixers/location_qualities.ts
@@ -1,5 +1,5 @@
 import {SettingsObject} from "../settings.js";
-import {IStateAware} from "./base";
+import {IMutationAwareFixer, IStateAware} from "./base";
 import {FLPlayerLocation, GameStateController} from "../game_state.js";
 
 type LocationPredicate = (location: FLPlayerLocation) => boolean
@@ -19,8 +19,9 @@ QUALITIY_LOCATION_PREDICATES.set("A Turncoat", isArea("Spite"));
 QUALITIY_LOCATION_PREDICATES.set("Disgruntlement among the Students", isSetting("Science Laboratory"));
 
 
-export class LocationQualitiesFixer implements IStateAware {
+export class LocationQualitiesFixer implements IMutationAwareFixer, IStateAware {
     private hideNonlocalQualities = false;
+    private currentLocation: FLPlayerLocation | null = null;
 
     applySettings(settings: SettingsObject): void {
         this.hideNonlocalQualities = settings.hide_nonlocal_qualities as boolean;
@@ -28,21 +29,41 @@ export class LocationQualitiesFixer implements IStateAware {
 
     linkState(state: GameStateController): void {
         state.onLocationChanged((location) => {
-            const sidebarQualities = document.querySelectorAll("div[class*='sidebar'] ul[class*='items--list'] li[class*='sidebar-quality']") as NodeListOf<HTMLElement>;
-            for (const quality of sidebarQualities) {
-                const qualityName = quality.querySelector("span[class*='item__name']");
-                if (!qualityName) continue;
-
-                const is_visible_at = QUALITIY_LOCATION_PREDICATES.get(qualityName.textContent || "");
-                if (!is_visible_at) continue;
-
-                if (is_visible_at(location)) {
-                    quality.style.cssText = "";
-                } else {
-                    quality.style.cssText = "display: none";
-                }
+            this.currentLocation = location;
+            if (this.hideNonlocalQualities) {
+                this.processSidebarQualities(location);
             }
         });
+    }
+
+    private processSidebarQualities(location: FLPlayerLocation) {
+        const sidebarQualities = document.querySelectorAll("div[class*='sidebar'] ul[class*='items--list'] li[class*='sidebar-quality']") as NodeListOf<HTMLElement>;
+        for (const quality of sidebarQualities) {
+            const qualityName = quality.querySelector("span[class*='item__name']");
+            if (!qualityName) continue;
+
+            const is_visible_at = QUALITIY_LOCATION_PREDICATES.get(qualityName.textContent || "");
+            if (!is_visible_at) continue;
+
+            if (is_visible_at(location)) {
+                quality.style.cssText = "";
+            } else {
+                quality.style.cssText = "display: none";
+            }
+        }
+    }
+
+    checkEligibility(node: HTMLElement): boolean {
+        return this.hideNonlocalQualities;
+    }
+
+    onNodeAdded(node: HTMLElement): void {
+        if (this.currentLocation) {
+            this.processSidebarQualities(this.currentLocation);
+        }
+    }
+
+    onNodeRemoved(node: HTMLElement): void {
     }
 
 }
