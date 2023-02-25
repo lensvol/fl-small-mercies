bbd5be33e8bcfa862d0d0ef41867cd2bef5bb5c1
Kirill Borisov <lensvol@gmail.com>
2023-02-25 01:17:15 +0100

    Restrict display of more qualities to their relevant location.

--- src/fixers/location_qualities.ts
+++ src/fixers/location_qualities.ts
@@ -2,8 +2,22 @@ import {SettingsObject} from "../settings.js";
 import {IStateAware} from "./base";
 import {FLPlayerLocation, GameStateController} from "../game_state.js";
 
-const QUALITIY_LOCATION_PREDICATES = new Map();
-QUALITIY_LOCATION_PREDICATES.set("Moonlit", (loc: FLPlayerLocation) => loc.area.areaId == 111141);
+type LocationPredicate = (location: FLPlayerLocation) => boolean
+
+function isArea(areaName: string): LocationPredicate {
+    return (location: FLPlayerLocation) => location.area.name == areaName
+}
+
+function isSetting(settingName: string): LocationPredicate {
+    return (location: FLPlayerLocation) => location.setting.name == settingName
+}
+
+const QUALITIY_LOCATION_PREDICATES = new Map<string, LocationPredicate>();
+QUALITIY_LOCATION_PREDICATES.set("Moonlit", isArea("Balmoral"));
+QUALITIY_LOCATION_PREDICATES.set("Taimen's Attention", isSetting("Khanate (Inner)"));
+QUALITIY_LOCATION_PREDICATES.set("A Turncoat", isArea("Spite"));
+QUALITIY_LOCATION_PREDICATES.set("Disgruntlement among the Students", isSetting("Science Laboratory"));
+
 
 export class LocationQualitiesFixer implements IStateAware {
     private hideNonlocalQualities = false;
@@ -19,10 +33,10 @@ export class LocationQualitiesFixer implements IStateAware {
                 const qualityName = quality.querySelector("span[class*='item__name']");
                 if (!qualityName) continue;
 
-                const is_visible_at = QUALITIY_LOCATION_PREDICATES.get(qualityName.textContent);
+                const is_visible_at = QUALITIY_LOCATION_PREDICATES.get(qualityName.textContent || "");
                 if (!is_visible_at) continue;
 
-                if (is_visible_at && is_visible_at(location)) {
+                if (is_visible_at(location)) {
                     quality.style.cssText = "";
                 } else {
                     quality.style.cssText = "display: none";
