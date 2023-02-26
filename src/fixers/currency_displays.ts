eed2051c981cd56f71f6abaf323e77faa74723b3
Kirill Borisov <lensvol@gmail.com>
2023-02-26 16:18:24 +0100

    FEATURE: Display more currencies in the left sidebar.

---
+++ src/fixers/currency_displays.ts
@@ -0,0 +1,69 @@
+import {SettingsObject} from "../settings.js";
+import {IMutationAwareFixer, IStateAware} from "./base";
+import {FLPlayerLocation, GameStateController} from "../game_state.js";
+
+type LocationPredicate = (location: FLPlayerLocation) => boolean
+
+function isArea(areaName: string): LocationPredicate {
+    return (location: FLPlayerLocation) => location.area.name == areaName
+}
+
+function isSetting(settingName: string): LocationPredicate {
+    return (location: FLPlayerLocation) => location.setting.name == settingName
+}
+
+const QUALITIY_LOCATION_PREDICATES = new Map<string, LocationPredicate>();
+QUALITIY_LOCATION_PREDICATES.set("Moonlit", isArea("Balmoral"));
+QUALITIY_LOCATION_PREDICATES.set("Taimen's Attention", isSetting("Khanate (Inner)"));
+QUALITIY_LOCATION_PREDICATES.set("A Turncoat", isArea("Spite"));
+QUALITIY_LOCATION_PREDICATES.set("Disgruntlement among the Students", isSetting("Science Laboratory"));
+
+
+export class LocationQualitiesFixer implements IMutationAwareFixer, IStateAware {
+    private hideNonlocalQualities = false;
+    private currentLocation: FLPlayerLocation | null = null;
+
+    applySettings(settings: SettingsObject): void {
+        this.hideNonlocalQualities = settings.hide_nonlocal_qualities as boolean;
+    }
+
+    linkState(state: GameStateController): void {
+        state.onLocationChanged((location) => {
+            this.currentLocation = location;
+            if (this.hideNonlocalQualities) {
+                this.processSidebarQualities(location);
+            }
+        });
+    }
+
+    private processSidebarQualities(location: FLPlayerLocation) {
+        const sidebarQualities = document.querySelectorAll("div[class*='sidebar'] ul[class*='items--list'] li[class*='sidebar-quality']") as NodeListOf<HTMLElement>;
+        for (const quality of sidebarQualities) {
+            const qualityName = quality.querySelector("span[class*='item__name']");
+            if (!qualityName) continue;
+
+            const is_visible_at = QUALITIY_LOCATION_PREDICATES.get(qualityName.textContent || "");
+            if (!is_visible_at) continue;
+
+            if (is_visible_at(location)) {
+                quality.style.cssText = "";
+            } else {
+                quality.style.cssText = "display: none";
+            }
+        }
+    }
+
+    checkEligibility(node: HTMLElement): boolean {
+        return this.hideNonlocalQualities;
+    }
+
+    onNodeAdded(node: HTMLElement): void {
+        if (this.currentLocation) {
+            this.processSidebarQualities(this.currentLocation);
+        }
+    }
+
+    onNodeRemoved(node: HTMLElement): void {
+    }
+
+}
