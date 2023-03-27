0eed44702a5251904889ea5d2c272a25523f9f5f
Kirill Borisov <lensvol@gmail.com>
2023-03-27 20:53:12 +0200

    Update selector for new 'Mask of the Rose' banner.

--- src/fixers/right_sidebar.ts
+++ src/fixers/right_sidebar.ts
@@ -1,7 +1,7 @@
 import {IMutationAwareFixer} from "./base.js";
 import {SettingsObject} from "../settings.js";
 
-const MASK_ROSE_BANNER_SELECTOR = "img[aria-label='Wishlist Mask of the Rose on Steam'"
+const MASK_ROSE_BANNER_SELECTOR = "div[class='travel'] a[target='_blank'][rel='noopener noreferrer'] img"
 const SNIPPET_CONTAINER_SELECTOR = "div[class='snippet']"
 
 export class RightSidebarFixer implements IMutationAwareFixer {
