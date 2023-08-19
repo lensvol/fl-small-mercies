336aa20609da13743e801827d207a3c5a84fb9fa
Kirill Borisov <lensvol@gmail.com>
2023-08-19 08:42:12 +0200

    Move token tracking back to API interceptor to remove circular dependency.

--- src/api_client.ts
+++ src/api_client.ts
@@ -1,5 +1,4 @@
 import {GameStateController, UnknownUser} from "./game_state.js";
-import { IUserResponse } from "./interfaces";
 
 const API_ROOT_URL = "https://api.fallenlondon.com/api";
 
@@ -33,17 +32,11 @@ export class FLApiClient {
     }
 
     public shareToProfile(contentKey: number, image?: string): Promise<any> {
-        const promise = this.callApi("POST", "/profile/share", {
+        return this.callApi("POST", "/profile/share", {
             contentClass: "EventConclusion",
             contentKey: contentKey,
             image: image ?? "snowflake",
             message: "Hello, world!",
-        });
-
-        promise.then((r: IUserResponse) => {
-            console.debug(r);
-        });
-
-        return promise;
+        })
     }
 }
