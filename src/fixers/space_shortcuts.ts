c174944d83df2eb738ae8ffddfa9c09f1e364c01
Kirill Borisov <lensvol@gmail.com>
2023-08-26 19:57:26 +0200

    Run `prettier` over whole codebase.

--- src/fixers/space_shortcuts.ts
+++ src/fixers/space_shortcuts.ts
@@ -1,8 +1,8 @@
-import { INetworkAware } from "./base.js";
-import { SettingsObject } from "../settings.js";
-import { DO_NOT_CARE, FLApiInterceptor, setFakeXhrResponse, SPECIAL_HANDLING } from "../api_interceptor.js";
-import { FLApiClient } from "../api_client.js";
-import { IBeginStoryletRequest } from "../interfaces.js";
+import {INetworkAware} from "./base.js";
+import {SettingsObject} from "../settings.js";
+import {DO_NOT_CARE, FLApiInterceptor, setFakeXhrResponse, SPECIAL_HANDLING} from "../api_interceptor.js";
+import {FLApiClient} from "../api_client.js";
+import {IBeginStoryletRequest} from "../interfaces.js";
 
 const STORYLET_TO_BRANCH_SHORTCUTS: Map<number, number> = new Map([
     // Put to Zee! -> Into the captain's cabin
@@ -12,7 +12,7 @@ const STORYLET_TO_BRANCH_SHORTCUTS: Map<number, number> = new Map([
     // Leave the Bone Market -> Relatively unnoticed
     [330284, 242571],
     // Stop by your Laboratory -> Go on in
-    [321218, 236273]
+    [321218, 236273],
 ]);
 
 export class SpaceShortcutFixer implements INetworkAware {
@@ -34,15 +34,11 @@ export class SpaceShortcutFixer implements INetworkAware {
             const beginRequest = data as unknown as IBeginStoryletRequest;
             const branchShortcut = STORYLET_TO_BRANCH_SHORTCUTS.get(beginRequest.eventId);
             if (branchShortcut) {
-                this.apiClient?.beginStorylet(beginRequest.eventId).then(
-                    (_) => {
-                        this.apiClient?.chooseBranch(branchShortcut).then(
-                            (json) => {
-                                setFakeXhrResponse(request, 200, json);
-                            }
-                        )
-                    }
-                )
+                this.apiClient?.beginStorylet(beginRequest.eventId).then((_) => {
+                    this.apiClient?.chooseBranch(branchShortcut).then((json) => {
+                        setFakeXhrResponse(request, 200, json);
+                    });
+                });
 
                 return SPECIAL_HANDLING;
             }
