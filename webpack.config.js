a74638df0aa23955466bb062ae8808832cd3bf48
Kirill Borisov <lensvol@gmail.com>
2023-09-24 11:10:39 +0200

    Output better source maps for generated bundles.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    devtool: "inline-source-map",
+    devtool: "source-map",
     entry: {
         payload: "./src/inject.ts",
         background: "./src/background.ts",
