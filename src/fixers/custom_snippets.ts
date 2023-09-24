cdf1d4a81fd21b152496e5d9423a5551cb1054a7
Kirill Borisov <lensvol@gmail.com>
2023-09-24 11:10:00 +0200

    Disable 'share snippet' button when displaying custom snippets.

--- src/fixers/custom_snippets.ts
+++ src/fixers/custom_snippets.ts
@@ -1,15 +1,37 @@
-import {INetworkAware} from "./base";
+import {IMutationAware, INetworkAware} from "./base";
 import {SettingsObject} from "../settings";
 import {FLApiInterceptor} from "../api_interceptor";
 import {IInfobarResponse, ISnippet} from "../interfaces";
 import {COMMUNITY_SNIPPETS} from "../constants";
+import {getSingletonByClassName} from "../utils";
 
-export class CustomSnippetsFixer implements INetworkAware {
-    // FIXME: Implement hiding of "record snippet" button on custom ones
-
+export class CustomSnippetsFixer implements INetworkAware, IMutationAware {
     enableCustomSnippets = true;
     customSnippets: ISnippet[];
     replaceDefaultSnippets = true;
+
+    snippetObserver = new MutationObserver((mutations) => {
+        this.hideEditButtonIfNeeded(mutations[0].target as HTMLElement);
+    });
+
+    private hideEditButtonIfNeeded(snippetContainer: HTMLElement) {
+        const mark = getSingletonByClassName(snippetContainer as HTMLElement, "community-mark");
+        const editBtn = getSingletonByClassName(snippetContainer as HTMLElement, "buttonlet-edit");
+
+        if (editBtn) {
+            if (mark) {
+                editBtn.classList.toggle("buttonlet-enabled", false);
+                editBtn.setAttribute("title", "Community snippets cannot be saved to your Journal.");
+                editBtn.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                });
+            } else {
+                editBtn.classList.toggle("buttonlet-enabled", true);
+                editBtn.setAttribute("title", "");
+            }
+        }
+    }
 
     constructor() {
         let i = 0;
@@ -17,7 +39,9 @@ export class CustomSnippetsFixer implements INetworkAware {
         this.customSnippets = COMMUNITY_SNIPPETS.map((snippet) => {
             i += 1;
             const textWithMention =
-                snippet.description + `<p><i>(by <b><a href="${snippet.link}">${snippet.author}</a></b>)</i></p>`;
+                snippet.description +
+                `<p><i>(by <b><a href="${snippet.link}">${snippet.author}</a></b>)</i></p>` +
+                '<p hidden aria-hidden="true" class="community-mark" />';
 
             return {
                 id: 777_777_777 + i,
@@ -53,4 +77,20 @@ export class CustomSnippetsFixer implements INetworkAware {
         interceptor.onResponseReceived("/api/infobar", snippetModifier);
         interceptor.onResponseReceived("/api/infobar/snippets", snippetModifier);
     }
+
+    checkEligibility(node: HTMLElement): boolean {
+        return (
+            getSingletonByClassName(node, "snippet") != null || getSingletonByClassName(node, "buttonlet-edit") != null
+        );
+    }
+
+    onNodeAdded(node: HTMLElement): void {
+        const snippetContainer = getSingletonByClassName(node, "snippet");
+        if (snippetContainer) {
+            this.snippetObserver.observe(snippetContainer, {childList: true, subtree: true});
+            this.hideEditButtonIfNeeded(snippetContainer);
+        }
+    }
+
+    onNodeRemoved(node: HTMLElement): void {}
 }
