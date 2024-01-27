c1243b8546a106095cc7e6f739052ed5d32c7455
Kirill Borisov <lensvol@gmail.com>
2024-01-27 16:19:59 +0100

    Write more interfaces to cover branch result types.

--- src/fixers/branch_results.ts
+++ src/fixers/branch_results.ts
@@ -1,6 +1,7 @@
 import {INetworkAware} from "./base";
 import {SettingsObject} from "../settings";
 import {FLApiInterceptor} from "../api_interceptor";
+import {IChooseBranchRequest, IChooseBranchResponse} from "../interfaces";
 
 export class BranchResultsFixer implements INetworkAware {
     apiInterceptor: FLApiInterceptor | null = null;
@@ -16,44 +17,47 @@ export class BranchResultsFixer implements INetworkAware {
     linkNetworkTools(interceptor: FLApiInterceptor): void {
         this.apiInterceptor = interceptor;
 
-        interceptor.onResponseReceived("/api/storylet/choosebranch", (request, response) => {
-            if (!response.messages) {
+        interceptor.onResponseReceived(
+            "/api/storylet/choosebranch",
+            (request: IChooseBranchRequest, response: IChooseBranchResponse) => {
+                if (!response.messages) {
+                    return;
+                }
+
+                const resultingMessages = [];
+
+                for (let message of response.messages) {
+                    if (
+                        this.removeQualityCapMessages &&
+                        message.type === "QualityCapMessage" &&
+                        message.changeType === "Unaltered"
+                    ) {
+                        continue;
+                    }
+
+                    if (this.removeSimpleChallengeText) {
+                        if (message.type === "DifficultyRollSuccessMessage") {
+                            message.message = message.message.replace(
+                                "(Simple challenges mean you don't learn so much.)",
+                                ""
+                            );
+                        }
+
+                        if (message.type === "DifficultyRollFailureMessage") {
+                            message.message = message.message.replace(
+                                "(This challenge was old territory for you - you won't learn so much.)",
+                                ""
+                            );
+                        }
+                    }
+
+                    resultingMessages.push(message);
+                }
+
+                response.messages = resultingMessages;
+
                 return;
             }
-
-            const resultingMessages = [];
-
-            for (let message of response.messages) {
-                if (
-                    this.removeQualityCapMessages &&
-                    message.type === "QualityCapMessage" &&
-                    message.changeType === "Unaltered"
-                ) {
-                    continue;
-                }
-
-                if (this.removeSimpleChallengeText) {
-                    if (message.type === "DifficultyRollSuccessMessage") {
-                        message.message = message.message.replace(
-                            "(Simple challenges mean you don't learn so much.)",
-                            ""
-                        );
-                    }
-
-                    if (message.type === "DifficultyRollFailureMessage") {
-                        message.message = message.message.replace(
-                            "(This challenge was old territory for you - you won't learn so much.)",
-                            ""
-                        );
-                    }
-                }
-
-                resultingMessages.push(message);
-            }
-
-            response.messages = resultingMessages;
-
-            return;
-        });
+        );
     }
 }
