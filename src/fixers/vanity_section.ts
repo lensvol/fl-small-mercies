76e8445751985b43984699e287264baae162e8dd
Kirill Borisov <lensvol@gmail.com>
2024-02-05 12:08:18 +0100

    Hide not-completed vanity qualities in a more responsive way.

--- src/fixers/vanity_section.ts
+++ src/fixers/vanity_section.ts
@@ -1,4 +1,4 @@
-import {INetworkAware} from "./base";
+import {IMutationAware, INetworkAware} from "./base";
 import {SettingsObject} from "../settings";
 import {FLApiInterceptor} from "../api_interceptor";
 import {IQuality} from "../interfaces";
@@ -19,13 +19,13 @@ const VANITY_QUALITY_IDS = [
     [777, 140888], // Palaeontologist
     [777, 141368], // Barrister of the Evenlode
 
-    // 143753], // Palaeozoologist (does not yet have QLDs)
+    // [???, 143753], // Palaeozoologist (does not yet have QLDs)
 
     [777, 143306], // Prolific Advertiser
     [777, 140904], // Record of Successful Forgery
     [777, 143573], // Scintillack Dreaming
 
-    // 142753], // Strength of your Khaganian Network (does not yet have QLDs)
+    // [???, 142753], // Strength of your Khaganian Network (does not yet have QLDs)
 
     [777, 127253], // Teaching Reputation of Your Laboratory
     [777, 127257], // The Prestige of your Laboratory
@@ -57,9 +57,10 @@ const VANITY_QUALITY_IDS = [
     [55, 144064], // Respected by Corsairs
 ];
 
-export class VanitySectionFixer implements INetworkAware {
+export class VanitySectionFixer implements INetworkAware, IMutationAware {
     showVanitySection = false;
     onlyCompletedVanities = true;
+    incompleteVanities: number[] = [];
 
     applySettings(settings: SettingsObject): void {
         this.showVanitySection = settings.show_vanity_section as boolean;
@@ -89,6 +90,7 @@ export class VanitySectionFixer implements INetworkAware {
             }
 
             const extractedVanities: IQuality[] = [];
+            this.incompleteVanities = [];
 
             for (const category of response.possessions) {
                 for (const quality of category.possessions) {
@@ -96,9 +98,10 @@ export class VanitySectionFixer implements INetworkAware {
                     if (!descriptor) continue;
 
                     const boundary = this.onlyCompletedVanities ? descriptor[0] : 1;
-                    if (boundary > quality.level) continue;
+                    if (boundary > quality.level) {
+                        this.incompleteVanities.push(quality.id);
+                    }
 
-                    // FIXME: Also remove the quality from the original category (make it configurable?).
                     extractedVanities.push(quality);
                 }
             }
@@ -111,5 +114,53 @@ export class VanitySectionFixer implements INetworkAware {
 
             return;
         });
+    }
+
+    onNodeAdded(node: HTMLElement): void {
+        if (!this.onlyCompletedVanities) {
+            return;
+        }
+
+        const qualityGroups = node.getElementsByClassName("quality-group");
+        if (qualityGroups.length == 0) {
+            return;
+        }
+
+        // Since /myself endpoint is only queried once, we need to manually hide the incomplete vanities
+        // using UI trickery.
+        for (const element of qualityGroups) {
+            const group = element as HTMLElement;
+            if (group.dataset.groupName !== "Vanities") {
+                continue;
+            }
+
+            for (const qualityIcon of group.getElementsByClassName("icon")) {
+                const iconElement = qualityIcon as HTMLElement;
+
+                if (!iconElement || !iconElement.parentElement) {
+                    continue;
+                }
+
+                // FL UI reuses "data-branch-id" for quality IDs on "Myself" tab.
+                const qualityId = parseInt(iconElement.dataset.branchId || "0");
+
+                if (this.incompleteVanities.includes(qualityId)) {
+                    // We need to hide the parent element, not the icon itself.
+                    iconElement.parentElement.style.cssText = "display: none";
+                } else {
+                    iconElement.parentElement.style.cssText = "";
+                }
+            }
+        }
+    }
+
+    onNodeRemoved(node: HTMLElement): void {}
+
+    checkEligibility(node: HTMLElement): boolean {
+        if (!this.showVanitySection || !this.onlyCompletedVanities) {
+            return false;
+        }
+
+        return node.getElementsByClassName("quality-group").length > 0;
     }
 }
