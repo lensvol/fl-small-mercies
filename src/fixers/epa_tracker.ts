5a8691e59e9f88d43113ab56cf1d7973686669aa
Kirill Borisov <lensvol@gmail.com>
2026-07-04 15:55:27 +0200

    Implement proper EPA tracker element in the currency section.

--- src/fixers/epa_tracker.ts
+++ src/fixers/epa_tracker.ts
@@ -1,7 +1,6 @@
-import {INetworkAware, IStateAware} from "./base";
+import {IMutationAware, INetworkAware, IStateAware} from "./base";
 import {SettingsObject} from "../settings";
 import {GameStateController} from "../game_state";
-import {debug} from "../logging";
 import {ITEM_PRICES_BY_ID} from "../datasets/item_prices";
 import {FLApiInterceptor} from "../api_interceptor";
 import {IChooseBranchResponse} from "../interfaces";
@@ -45,15 +44,143 @@ class EPATracker {
     public reset() {
         this.sessionWealth = 0;
         this.sessionActions = 0;
-
-        debug("EPA tracker has been reset.");
     }
 }
 
-export class EpaTrackerFixer implements IStateAware, INetworkAware {
+function createEpaTrackerMimic(): [
+    HTMLDivElement,
+    HTMLSpanElement,
+    HTMLSpanElement,
+    HTMLAnchorElement,
+    HTMLAnchorElement
+] {
+    const root = document.createElement("div");
+
+    const li = document.createElement("li");
+    li.classList.add("item");
+
+    const img = document.createElement("img");
+    img.classList.add("media__object");
+    img.setAttribute("height", "78");
+    img.setAttribute("width", "60");
+    img.setAttribute("alt", "");
+    img.setAttribute("src", "//images.fallenlondon.com/currencies/echoes.png");
+    img.setAttribute("aria-label", "");
+
+    const containingDiv = document.createElement("div");
+    containingDiv.classList.add("icon--currency", "sidebar__echoes-button", "sidebar__button--has-focus-outline");
+
+    const textSpan = document.createElement("span");
+    textSpan.classList.add("u-visually-hidden");
+
+    const container = document.createElement("div");
+    container.classList.add("item__desc");
+
+    const text = document.createTextNode("Open the Bazaar tab");
+
+    const textSpan2 = document.createElement("span");
+    textSpan2.classList.add("js-item-name", "item__name");
+
+    const container2 = document.createElement("div");
+    container2.classList.add("item__value");
+
+    const text2 = document.createTextNode("EPA tracker");
+
+    const container3 = document.createElement("div");
+    container3.classList.add("price", "item__price", "price--inverted");
+
+    const epa = document.createElement("span");
+
+    const br = document.createElement("br");
+
+    const info = document.createElement("span");
+    info.style.cssText = "font-size: smaller";
+
+    const paragraph = document.createElement("br");
+
+    const text3 = document.createTextNode("7,4");
+
+    const text4 = document.createTextNode("123 Echoes / 14 Actions");
+
+    // Weird hyperlink here is used to prevent window frame from "jumping" to the
+    // top when the link is clicked upon (https://stackoverflow.com/a/39112476)
+    const trackerToggle = document.createElement("a");
+    trackerToggle.setAttribute("href", "javascript:void(0)");
+
+    const text5 = document.createTextNode(" • ");
+
+    const trackerReset = document.createElement("a");
+    trackerReset.setAttribute("href", "javascript:void(0)");
+
+    const text6 = document.createTextNode("Start");
+
+    const text7 = document.createTextNode("Reset");
+
+    root.appendChild(li);
+
+    li.appendChild(containingDiv);
+    li.appendChild(textSpan);
+    li.appendChild(container);
+
+    containingDiv.appendChild(img);
+
+    textSpan.appendChild(text);
+
+    container.appendChild(textSpan2);
+    container.appendChild(container2);
+
+    textSpan2.appendChild(text2);
+
+    container2.appendChild(container3);
+
+    container3.appendChild(epa);
+    container3.appendChild(br);
+    container3.appendChild(info);
+    container3.appendChild(paragraph);
+    container3.appendChild(trackerToggle);
+    container3.appendChild(text5);
+    container3.appendChild(trackerReset);
+
+    epa.appendChild(text3);
+
+    info.appendChild(text4);
+
+    trackerToggle.appendChild(text6);
+
+    trackerReset.appendChild(text7);
+
+    return [root, epa, info, trackerToggle, trackerReset];
+}
+
+export class EpaTrackerFixer implements IStateAware, INetworkAware, IMutationAware {
     private showEpaTracker = false;
+    private areWeTracking = false;
     private currentActions = 0;
     private epaTracker = new EPATracker();
+
+    private trackerUiMimic: HTMLDivElement;
+    private epaIndicator: HTMLSpanElement;
+    private epaInfoLine: HTMLSpanElement;
+    private trackerToggle: HTMLAnchorElement;
+    private trackerReset: HTMLAnchorElement;
+
+    constructor() {
+        const mimicParts = createEpaTrackerMimic();
+        this.trackerUiMimic = mimicParts[0];
+        this.epaIndicator = mimicParts[1];
+        this.epaInfoLine = mimicParts[2];
+        this.trackerToggle = mimicParts[3];
+        this.trackerReset = mimicParts[4];
+
+        this.trackerReset.addEventListener("click", () => {
+            this.resetTracker();
+        });
+
+        this.trackerToggle.addEventListener("click", () => {
+            this.areWeTracking = !this.areWeTracking;
+            this.trackerToggle.textContent = this.areWeTracking ? "Stop" : "Start";
+        });
+    }
 
     applySettings(settings: SettingsObject): void {
         this.showEpaTracker = settings.show_treasure_marker as boolean;
@@ -63,7 +190,6 @@ export class EpaTrackerFixer implements IStateAware, INetworkAware {
         state.onActionsChanged((g, actionsLeft) => {
             const actionDiff = this.currentActions - actionsLeft;
             if (actionDiff > 0) {
-                debug(`This branch took ${actionDiff}`);
                 this.epaTracker.increaseActions(actionDiff);
             }
 
@@ -73,7 +199,7 @@ export class EpaTrackerFixer implements IStateAware, INetworkAware {
 
     linkNetworkTools(interceptor: FLApiInterceptor): void {
         interceptor.onResponseReceived("/api/storylet/choosebranch", (_, response: IChooseBranchResponse) => {
-            if (!this.showEpaTracker || !response.isSuccess) return;
+            if (!this.showEpaTracker || !this.areWeTracking || !response.isSuccess) return;
 
             for (const message of response.messages) {
                 if (message.type === "StandardQualityChangeMessage" && message.changeType !== "Unaltered") {
@@ -92,20 +218,39 @@ export class EpaTrackerFixer implements IStateAware, INetworkAware {
                         (wasIncreased ? 1 : -1);
 
                     this.epaTracker.increaseWealth(amountChanged);
-
-                    const actionDiff = this.currentActions - response.actions;
-                    if (actionDiff > 0) {
-                        this.epaTracker.increaseActions(actionDiff);
-                    } else {
-                        this.epaTracker.increaseActions(1);
-                    }
-
-                    debug(`You've ${wasIncreased ? "gained" : "lost"} ${amountChanged} Echoes`);
+                    this.updateTrackerUI();
                 }
             }
 
-            const epa = this.epaTracker.getEPA();
-            debug(`Current EPA: ${this.epaTracker.getWealth()} / ${this.epaTracker.getActionCount()} = ${epa}`);
+            const epa = this.epaTracker.getEPA().toFixed(2);
+            this.updateTrackerUI();
         });
     }
+
+    private resetTracker() {
+        this.epaTracker.reset();
+        this.epaIndicator.textContent = "0,0";
+        this.epaInfoLine.textContent = "0E / ??? Action(s)";
+    }
+
+    private updateTrackerUI() {
+        this.epaIndicator.textContent = String(this.epaTracker.getEPA());
+        this.epaInfoLine.textContent = `${this.epaTracker
+            .getWealth()
+            .toFixed(2)}E / ${this.epaTracker.getActionCount()} Action(s)`;
+    }
+
+    checkEligibility(node: HTMLElement): boolean {
+        return node.querySelector("div[class='col-secondary sidebar'] ul[class*='items--list']") != null;
+    }
+
+    onNodeAdded(node: HTMLElement): void {
+        const currencyList = node.querySelector("div[class='col-secondary sidebar'] ul[class*='items--list']");
+        if (!currencyList) return;
+
+        currencyList.appendChild(this.trackerUiMimic);
+        this.resetTracker();
+    }
+
+    onNodeRemoved(node: HTMLElement): void {}
 }
