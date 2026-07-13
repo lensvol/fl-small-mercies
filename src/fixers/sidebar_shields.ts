7c6623dfe71db048476200c67158040ea5aea117
Kirill Borisov <lensvol@gmail.com>
2026-07-13 11:55:29 +0200

    Displays ability changes in real-time when using compact mode.

--- src/fixers/sidebar_shields.ts
+++ src/fixers/sidebar_shields.ts
@@ -2,19 +2,38 @@ import {IMutationAware, IStateAware} from "./base";
 import {SettingsObject} from "../settings";
 import {getSingletonByClassName} from "../utils";
 import {GameStateController} from "../game_state";
+import {debug} from "../logging";
 
 class SidebarShield {
     private level: number = 0;
     private modifier: number = 0;
     private imageName: string;
+    private container: HTMLDivElement;
+    private levelDisplay: HTMLSpanElement;
 
     constructor(image: string, level: number = 0, modifier: number = 0) {
         this.imageName = image;
-        this.level = level;
-        this.modifier = modifier;
+        this.setLevel(level, modifier);
+        this.container = this.render();
+        this.levelDisplay = getSingletonByClassName(this.container, "agent-stat-level")!!;
     }
 
-    render(): HTMLDivElement {
+    setLevel(level: number, modifier: number) {
+        this.level = level;
+        this.modifier = modifier;
+        if (this.levelDisplay) {
+            this.levelDisplay.textContent = (this.level + this.modifier).toString();
+        }
+    }
+
+    pulse() {
+        this.container.classList.add("pulse-golden-light");
+        setTimeout(() => {
+            this.container.classList.remove("pulse-golden-light");
+        }, 500);
+    }
+
+    private render(): HTMLDivElement {
         const container = document.createElement("div");
         container.classList.add("agent-stat-wrapper");
         container.setAttribute("role", "button");
@@ -30,6 +49,7 @@ class SidebarShield {
 
         const textSpan = document.createElement("span");
         textSpan.classList.add("agent-stat-level");
+        this.levelDisplay = textSpan;
 
         const text = document.createTextNode((this.level + this.modifier).toString());
 
@@ -41,6 +61,10 @@ class SidebarShield {
         textSpan.appendChild(text);
 
         return container;
+    }
+
+    getElement(): HTMLDivElement {
+        return this.container;
     }
 }
 
@@ -57,7 +81,7 @@ class SidebarShieldWall {
         container.style.marginTop = "10px";
         container.style.gridTemplateColumns = "repeat(auto-fill,minmax(32px,1fr))";
 
-        this.shields.forEach((shield) => container.appendChild(shield.render()));
+        this.shields.forEach((shield) => container.appendChild(shield.getElement()));
 
         return container;
     }
@@ -66,10 +90,11 @@ class SidebarShieldWall {
 export class SidebarShieldsFixer implements IMutationAware, IStateAware {
     private showShieldWall: boolean = false;
     private shieldWall = new SidebarShieldWall();
+    private abilityToShield: Map<number, SidebarShield> = new Map();
 
     linkState(state: GameStateController): void {
         state.onUserDataLoaded((state) => {
-            const abilityCategories = ["BasicAbility", "SidebarAbility", "Skills", "SidebarTransient"];
+            const abilityCategories = ["BasicAbility", "SidebarAbility", "Skills"];
 
             abilityCategories.map((categoryCode) => {
                 const sidebarCategory = state.getQualityCategory(categoryCode);
@@ -77,9 +102,47 @@ export class SidebarShieldsFixer implements IMutationAware, IStateAware {
                     return;
                 }
                 for (const quality of sidebarCategory) {
-                    this.shieldWall.addShield(new SidebarShield(quality.image, quality.effectiveLevel, 0));
+                    const shield = new SidebarShield(
+                        quality.image,
+                        quality.level,
+                        quality.effectiveLevel - quality.level
+                    );
+                    this.abilityToShield.set(quality.qualityId, shield);
+                    this.shieldWall.addShield(shield);
                 }
             });
+        });
+
+        state.onQualityChanged((state, quality, _prevLevel, _curLevel) => {
+            const existingShield = this.abilityToShield.get(quality.qualityId);
+            if (existingShield) {
+                existingShield.setLevel(quality.effectiveLevel, quality.effectiveLevel - quality.level);
+                existingShield.pulse();
+                debug("Quality should be updated", quality);
+            }
+        });
+
+        state.onEquipmentChanged((state, slotName, previous, current) => {
+            debug(`Equipment changed in slot ${slotName}: ${previous} -> ${current}`);
+
+            // TODO: Pulse different colors for addition and removal
+            const removedEnhancements = previous ? previous.enhancements : [];
+            const addedEnhancements = current ? current.enhancements : [];
+
+            for (const enhancement of [...removedEnhancements, ...addedEnhancements]) {
+                const existingShield = this.abilityToShield.get(enhancement.qualityId);
+                if (!existingShield) continue;
+
+                const quality = state.getQualityById(enhancement.qualityId);
+                if (!quality) {
+                    debug("Something went wrong, shield was found in enhancement but quality was not", quality);
+                    continue;
+                }
+
+                debug(`Updating shield for "${quality.name}"`, quality.level);
+                existingShield.setLevel(quality.level, quality.effectiveLevel - quality.level);
+                existingShield.pulse();
+            }
         });
     }
 
