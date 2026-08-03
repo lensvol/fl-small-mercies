3ae178b681b17501ef1e33496f0b389dbde49f05
Kirill Borisov <lensvol@gmail.com>
2026-08-03 13:13:30 +0200

    Re-engineer CP tracking to correctly calculate values when quality changes.

--- src/fixers/cp_annotation.ts
+++ src/fixers/cp_annotation.ts
@@ -7,70 +7,84 @@ import {sumArithmeticSequence} from "../utils";
 import {debug} from "../logging";
 import {PYRAMIDAL_QUALITY_IDS} from "../datasets/qualities";
 
-const CHANGE_POINTS_REGEX = /(\d+) change points, (\d+) more needed to reach level (\d+)/;
+const CHANGE_POINTS_REGEX = /(\d+) change points*, (\d+) more needed to reach level (\d+)/;
 
-function calculatePyramidalChangePoints(quality: Quality): number {
-    return (
-        sumArithmeticSequence(quality.level) + Math.round((quality.level + 1) * (quality.progressAsPercentage / 100))
-    );
+function calculateChangePoints(quality: Quality): number {
+    if (PYRAMIDAL_QUALITY_IDS.includes(quality.qualityId)) {
+        return sumArithmeticSequence(quality.level);
+    } else {
+        return quality.effectiveLevel;
+    }
 }
 
 export class ChangePointsAnnotationFixer implements INetworkAware, IStateAware {
     private qualityChangePoints: Map<number, number> = new Map();
+    private approximatedQualities: Set<number> = new Set();
 
     applySettings(settings: SettingsObject): void {}
 
     linkNetworkTools(interceptor: FLApiInterceptor): void {
         interceptor.onResponseReceived("/api/storylet/choosebranch", (_, response: IChooseBranchResponse) => {
             for (const message of response.messages || []) {
-                if (message.type !== "PyramidQualityChangeMessage") {
-                    // For the moment we are only concerned with the change of existing pyramidal qualities.
-                    continue;
+                if (message.type === "PyramidQualityChangeMessage" || message.type === "StandardQualityChangeMessage") {
+                    if (message.possession.nature === "Thing") {
+                        continue;
+                    }
+
+                    if (message.possession.allowedOn === "World") {
+                        continue;
+                    }
+
+                    let extractedPoints = 0;
+                    const matches = message.tooltip?.match(CHANGE_POINTS_REGEX);
+                    if (matches) {
+                        const [_, currentCP, _pointsNeeded, _nextLevel] = matches;
+                        debug(`Extracted following values from '${message.message}:`, matches);
+                        extractedPoints = Number.parseInt(currentCP);
+                    }
+
+                    const quality = Quality.fromJson(message.possession);
+                    const calculatedPoints = calculateChangePoints(quality);
+                    const oldPoints = this.qualityChangePoints.get(quality.qualityId) || 0;
+                    const delta = calculatedPoints + extractedPoints - oldPoints;
+
+                    if (oldPoints !== calculatedPoints + extractedPoints) {
+                        let approx = "";
+                        if (this.approximatedQualities.has(quality.qualityId)) {
+                            approx = "approximately ";
+                            this.approximatedQualities.delete(quality.qualityId);
+                        }
+
+                        const annotation = `(${approx}${delta > 0 ? "+" : "-"}${Math.abs(delta)} CP)`;
+                        message.message = `${message.message} <em>${annotation}</em>`;
+                        this.qualityChangePoints.set(message.possession.id, calculatedPoints + extractedPoints);
+                    }
+                }
+                if (message.type === "QualityExplicitlySetMessage") {
+                    const calculatedPoints = calculateChangePoints(Quality.fromJson(message.possession));
+                    this.qualityChangePoints.set(message.possession.id, calculatedPoints);
                 }
             }
+
+            return response;
         });
     }
 
     linkState(state: GameStateController): void {
         state.onCharacterDataLoaded((state: GameState) => {
             for (const quality of state.enumerateQualities()) {
+                let calculatedPoints = calculateChangePoints(quality);
+
                 if (quality.progressAsPercentage === -1) {
-                    // This quality is not progressing via change points.
-                    continue;
+                    // Sadly /myself response does not provide any information about the progress to the next level,
+                    // so we will have to explicitly inform user of the fact that we approximate initial value.
+                    this.approximatedQualities.add(quality.qualityId);
+                } else if (quality.progressAsPercentage > 0 && PYRAMIDAL_QUALITY_IDS.includes(quality.qualityId)) {
+                    calculatedPoints += Math.round((quality.level + 1) * (quality.progressAsPercentage / 100));
                 }
 
-                if (PYRAMIDAL_QUALITY_IDS.includes(quality.qualityId)) {
-                    const calculatedPoints = calculatePyramidalChangePoints(quality);
-                    this.qualityChangePoints.set(quality.qualityId, calculatedPoints);
-                    debug(`CP points for ${quality.name}: ${calculatedPoints}`, quality);
-                } else {
-                    this.qualityChangePoints.set(quality.qualityId, quality.level);
-                    debug(`CP points for ${quality.name}: ${quality.level}`, quality);
-                }
-            }
-        });
-
-        state.onQualityChanged((_state: GameState, _previous, quality) => {
-            if (quality.progressAsPercentage === -1) {
-                return;
-            }
-
-            const oldPoints = this.qualityChangePoints.get(quality.qualityId) || 0;
-            if (PYRAMIDAL_QUALITY_IDS.includes(quality.qualityId)) {
-                const calculatedPoints = calculatePyramidalChangePoints(quality);
                 this.qualityChangePoints.set(quality.qualityId, calculatedPoints);
-                debug(
-                    `Delta for quality ${quality.name}: ${calculatedPoints} - ${oldPoints} = ${
-                        calculatedPoints - oldPoints
-                    } CP`
-                );
-            } else {
-                this.qualityChangePoints.set(quality.qualityId, quality.level);
-                debug(
-                    `Delta for quality ${quality.name}: ${quality.level} - ${oldPoints} = ${
-                        quality.level - oldPoints
-                    } CP`
-                );
+                debug(`CP points for ${quality.name}: ${calculatedPoints}`);
             }
         });
     }
